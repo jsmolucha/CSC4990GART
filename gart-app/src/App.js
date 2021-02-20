d1737ae91e31365ab276534f71a22537fdc7cffe
Jakub Smolucha <56137917+jsmolucha@users.noreply.github.com>
2021-02-20 14:13:38 -0500

    jsx routes
    installed react router dom and configured the package-lock file. modified project structure and pruned un-needed files.

--- gart-app/src/App.js
+++ gart-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+       {/*All our Routes goes here!*/}
+       <Route path="/" component={} />
+      </Router>
+    );
+  }
 }
 
 export default App;
