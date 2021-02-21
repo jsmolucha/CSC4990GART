d3f8e04859ebfa196746cd9dc51536e050c2a433
Jakub Smolucha <56137917+jsmolucha@users.noreply.github.com>
2021-02-21 11:50:34 -0500

    started UI layout
    removed react README. started layout for the login screen. incorporated links and switches back to the different pages. using components.

--- gart-app/src/pages/index.jsx
+++ gart-app/src/pages/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.css"
 //Functional Component 
 const Welcome = () => {
   return (
-    <div>
-      <h1>OK so this is the welcome page </h1>
-      <h3>WELCOME PAGE</h3>
-      <Link to="/main">come on in</Link>
+    <div className="container">
+        <div className="logotype">GART </div>
+        <div className="logopic">logo here</div>
+        <div className="slogan">A creative forum for gaming art </div>
+        <div className="loginflow"> already with us? login!</div>
+        <div className="signupflow"> new here? signup!</div>
+        <Link className="backmain" to="/main">come on in</Link>
     </div>
   );
 };
