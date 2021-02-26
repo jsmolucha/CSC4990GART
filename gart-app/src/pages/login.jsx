3a67e19f056fa60de5cd96a94510a9b63bcb6ebc
Jakub Smolucha <56137917+jsmolucha@users.noreply.github.com>
2021-02-26 12:46:44 -0600

    code cleanup and styling
    added background placeholder and styling to login page.

--- gart-app/src/pages/login.jsx
+++ gart-app/src/pages/login.jsx
@@ -9,8 +9,8 @@ const login = () => {
             <div className="logincontainer">
                 <form className="loginForm" method="post" action = "login">
                     <h1 id="loginHeader"> LOGIN</h1>
-                    <input id="uNameField" type="text" placeholder="username" name="uname" required />
-                    <input id="pWordField" type="password" placeholder="password" name="psw" required/>
+                    <input id="uNameField" type="text" placeholder="Username" name="uname" required />
+                    <input id="pWordField" type="password" placeholder="Password" name="psw" required/>
                     <button id="submitLogin" type="submit">Login</button>
                 </form>
             </div>
