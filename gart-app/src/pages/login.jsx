a26a8e856e1568e3eedf3e3e6d4a35ae05a27d40
Jakub Smolucha <56137917+jsmolucha@users.noreply.github.com>
2021-03-01 10:48:37 -0600

    code cleanup

--- gart-app/src/pages/login.jsx
+++ gart-app/src/pages/login.jsx
@@ -9,13 +9,14 @@ const login = () => {
             <div className="logincontainer">
                 <form className="loginForm" method="post" action = "login">
                     <h1 id="loginHeader"> LOGIN</h1>
-                    <input id="uNameField" type="text" placeholder="Username" name="uname" required />
+                    <input id="uNameField" type="text" placeholder="Email" name="uname" required />
                     <input id="pWordField" type="password" placeholder="Password" name="psw" required/>
                     <button id="submitLogin" type="submit">Login</button>
+                    <p>Not registered? Sign up here!</p>
                 </form>
             </div>
             <div className="loginGreeting">
-                <h1 id="welcomeHeader">Welcome back, creative!</h1>
+                <h1 id="welcomeHeader">Welcome back,<span id="colorText"> creative!</span> </h1>
             </div>
 
         </div>
