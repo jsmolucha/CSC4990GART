762a908e28e8abe260a8b96439018ba6c3980268
Carlos <galvancarlosjr@gmail.com>
2021-03-13 23:06:16 -0600

    updated errors and main page
    if CORS error occurs use the extension that Jakub shared.

--- gart-app/src/actions/auth.js
+++ gart-app/src/actions/auth.js
@@ -9,8 +9,20 @@ export const signin = (formData, router) => async (dispatch) => {
     // alert("hello")
     router.push('/main');
   } catch (error) {
-    console.log(error);
-  }
+        if (error.response) {
+            // Request made and server responded
+            console.log(error.response.data);
+            alert(error.response.data)
+            console.log(error.response.status);
+            // console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+    }
 };
 
 // export const signup = (formData, router) => async (dispatch) => {
--- gart-app/src/pages/mainpage.jsx
+++ gart-app/src/pages/mainpage.jsx
@@ -1,8 +1,42 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
+// import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+// import { Link } from "react-router-dom";
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
+import * as actionType from '../constants/actionTypes';
+import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import useStyles from './styles/main';
 /* We simply can use an array and loop and print each user */
-const mainpage  = () => {
+const Mainpage  = () => {
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const history = useHistory();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const classes = useStyles();
+
+
+  const logout = () => {
+
+    dispatch({ type: actionType.LOGOUT });
+
+    history.push('/main');
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
 
   // const data = localStorage.getItem('profile')
   // const [value, setValue] = React.useState(
@@ -11,13 +45,29 @@ const mainpage  = () => {
   
   return (
     <div>
-      <h1>this is where the content will go</h1>
-      <ul>
-        <li>{localStorage.getItem('profile')}</li>
-      </ul>
+       {user?.result ? (
+          <div >
+            <h1>You are logged in.</h1>
+            <ul>
+              <li>Username: {user?.result.username}</li>
+              <li>ID: {user?.result._id}</li>
+              <li>Email: {user?.result.email}</li>
+            </ul>
+            {/* <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar> */}
+            {/* <Typography className={classes.userName} variant="h6">{user?.result.username}</Typography> */}
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+          </div>
+        ) : (
+          <div>
+            <h1>You are not logged in!</h1>
+            <Button component={Link} to="/login" variant="contained" color="primary">Sign In</Button>
+
+          </div>
+        )}
+
       <Link to="/">back to welcome</Link>
     </div>
   );
 };
 
-export default mainpage;
+export default Mainpage;
