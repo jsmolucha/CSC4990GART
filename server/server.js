312e4dfc246c47ea062ab73c47be6251595a7a59
Carlos <galvancarlosjr@gmail.com>
2021-03-15 10:09:21 -0500

    Small change

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(cors())
 
 app.use(function(req, res, next) {
 
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000/'); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   //These where added during testing... it may or may not break it, idk -carlos
