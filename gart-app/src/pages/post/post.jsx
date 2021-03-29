44b24b425f60fc75feea58d7f93e20c8753f88d3
Carlos <galvancarlosjr@gmail.com>
2021-03-29 01:10:18 -0500

    Saving point
    >Going to experiment with atomize

--- gart-app/src/pages/post/post.jsx
+++ gart-app/src/pages/post/post.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import axios from 'axios';
-import PostCard from './postCard/postCard'
-
+import axios from "axios";
+import PostCard from "./postCard/postCard";
+import Gallery from "react-grid-gallery";
+// import CardGallery from "react-card-image-gallery";
+import Masonry from "react-masonry-css";
+import styles from './styles.css'
 // const { API_URL } = require('../constants/constants')
-
 
 // const API = axios.create({ baseURL: `${API_URL}` });
 
@@ -15,72 +17,90 @@ class Post extends React.Component {
       isLoaded: false,
       images: [],
       owner: null,
-      setCurrentId: null
+      setCurrentId: null,
     };
   }
-  
-  
+
   componentDidMount() {
-      const {username}  = this.props.match.params
-      console.log(username)
-    let user = JSON.parse(localStorage.getItem('profile'));
+    const { username } = this.props.match.params;
+    console.log(username);
+    let user = JSON.parse(localStorage.getItem("profile"));
     // console.log("user:", user)
     // console.log(this)
-    axios.get(`http://localhost:5000/api/accounts/username/${username}`, {
-      params: {
-        // ID : user.result.userID
-      }
-    })
-    .then( res => {
-      // console.log(res.data,"response")
-      this.setState({
-        isLoaded: true,
-        images: res.data,
-        owner: username,
-        setCurrentId: user.result.userID
-      });
-    }, 
-    (error) => {
-      console.log("error")
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    }
-    )
+    axios
+      .get(`http://localhost:5000/api/accounts/username/${username}`, {
+        params: {
+          // ID : user.result.userID
+        },
+      })
+      .then(
+        (res) => {
+          // console.log(res.data,"response")
+          this.setState({
+            isLoaded: true,
+            images: res.data,
+            owner: username,
+            setCurrentId: user.result.userID,
+          });
+        },
+        (error) => {
+          console.log("error");
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
   }
-  
-  render() {
-  const { error, isLoaded, images } = this.state;
-  // return(<div onLoad={componenetDidMount()}>
-  //   Loading
-  // </div>)
-  
-  // </div>
-  // componenetDidMount()
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div >Loading... ok
-      {/* <div> {console.log(this.state)}</div> */}
-    </div>;
-  }
-  else {
-    return <div>
-      <h1>Welcome to {this.state.owner}' profile page</h1> 
-      {/* <Post post={post} setCurrentId={setCurrentId} /> */}
-      {this.state.images.map(p => {
-      return <div key={p._id}>
-           <PostCard post={p} setCurrentId={this.state.setCurrentId} />
-        {/* <img key={p._id} src={p.filePath} alt={`${p.title} by ${p.creator}`} />;
-        <h3>{`${p.title} by ${p.creator}`}</h3> */}
-        </div>
-    })}
-      {console.log(this.state.images)}
-      </div>
-  }
-}
 
+  render() {
+    const { error, isLoaded, images } = this.state;
+    const breakpointColumnsObj = {
+      default: 4,
+      1100: 3,
+      700: 2,
+      500: 1
+    };
+    // return(<div onLoad={componenetDidMount()}>
+    //   Loading
+    // </div>)
+
+    // </div>
+    // componenetDidMount()
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return (
+        <div>
+          Loading... ok
+          {/* <div> {console.log(this.state)}</div> */}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Welcome to {this.state.owner}' profile page</h1>
+          {/* <Post post={post} setCurrentId={setCurrentId} /> */}
+          <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {this.state.images.map((p) => {
+              return (
+                <div key={p._id}>
+                  <PostCard post={p} setCurrentId={this.state.setCurrentId} />
+                  {/* <img key={p._id} src={p.filePath} alt={`${p.title} by ${p.creator}`} />;
+        <h3>{`${p.title} by ${p.creator}`}</h3> */}
+                </div>
+              );
+            })}
+            </Masonry>
+          {/* {console.log(this.state.images)} */}
+        </div>
+      );
+    }
+  }
 }
 
 /*
@@ -103,4 +123,4 @@ const Accountpage  = () => {
   );
 };
 */
-export default Post; 
+export default Post;
--- gart-app/src/pages/post/postCard/styles.js
+++ gart-app/src/pages/post/postCard/styles.js
@@ -4,8 +4,8 @@ export default makeStyles({
   media: {
     height: 0,
     paddingTop: '56.25%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    backgroundBlendMode: 'darken',
+    // backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    // backgroundBlendMode: 'darken',
   },
   border: {
     border: 'solid',
