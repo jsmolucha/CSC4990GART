4dca2b41d91d1e304cc5f013d82ae8fc7e00906c
bjaynes01 <55394496+bjaynes01@users.noreply.github.com>
2021-04-13 19:26:08 -0500

    reorganizing

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,3 +5,25 @@
 //read --> displays user data; useful for login and for other
 //update --> updates the user info; corralates with an 'account' page on the front end side (tba)
 //delete user --->deletes user from collection permanantly; standard practice for all websites
+
+
+import express from "express";
+import mongoose from "mongoose";
+
+import users from "../models/users.js";
+import asyncHandler from "express-async-handler";
+
+import formidable from "formidable";
+const router = express.Router();
+
+// update
+export const UpdateProfile = async (req, res) => {
+    const { userID } = req.params;
+    try {
+
+  
+      res.status(200).json(postMessages);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  };
