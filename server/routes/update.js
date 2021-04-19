1a6c94409918d8941908cbeb9964d3d0db4df773
bjaynes01 <55394496+bjaynes01@users.noreply.github.com>
2021-04-19 09:19:18 -0500

    router update

--- server/routes/update.js
+++ server/routes/update.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import users from '../models/users.js'
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.post('/updateForm', loadUpdate);
-router.patch('/updateData', updateProfile)
+router.route('/users/edit').post((req,res) => {
+});
 
+router.route('/users/edit/done').post((req,res) => {
+
+});
 export default router;
 
